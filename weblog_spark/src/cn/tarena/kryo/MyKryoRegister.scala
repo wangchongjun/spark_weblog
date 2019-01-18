a3e7f1ade2b61bf0a6b43623e6e58d65d4b1af93
wcj <1043115682@qq.com>
2019-01-18 19:57:09 +0800

    ~~~~

--- weblog_spark/src/cn/tarena/kryo/MyKryoRegister.scala
+++ weblog_spark/src/cn/tarena/kryo/MyKryoRegister.scala
@@ -8,7 +8,7 @@ import cn.tarena.Tongji2
 
 class MyKryoRegister extends KryoRegistrator {
   
-  
+   //序列化   
   def registerClasses(kryo: Kryo): Unit = {
     //--将指定的类的序列化替换成kryo
     //--用这种方式，可以将指定的多个类的序列化替换
